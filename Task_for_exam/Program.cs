59ce9503c8b547bfc130f66afa3959dcf91f838d
Artem <letnev2012@gmail.com>
2022-08-07 18:32:22 +0300

    Add solution using methods

--- Task_for_exam/Program.cs
+++ Task_for_exam/Program.cs
@@ -33,3 +33,36 @@ Console.WriteLine($"[{String.Join("; ", finishArray)}]");
 
 
 
+
+// Solution using methods
+
+// string[] standartArray = { "3213", "22222", "W", "=3", "543" };
+// int lengthFinishArray = 0;
+// for (int i = 0; i < standartArray.Length; i++)
+// {
+//     if (standartArray[i].Length < 4)
+//     {
+//         lengthFinishArray++;
+//     }
+// }
+
+// string[] finishArray = new string[lengthFinishArray];
+
+// void FillingFinishArray(string[] standartArray, string[] finishArray)
+// {
+//     int indexFinishArray = 0;
+//     for (int i = 0; i < standartArray.Length; i++)
+//     {
+//         if (standartArray[i].Length < 4)
+//         {
+//             finishArray[indexFinishArray] = standartArray[i];
+//             indexFinishArray++;
+//         }
+//     }
+// }
+// FillingFinishArray(standartArray, finishArray);
+
+// Console.WriteLine($"[{String.Join("; ", finishArray)}]");
+
+
+
