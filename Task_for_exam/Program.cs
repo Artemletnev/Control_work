f3ad251fc1b2d30f2070dd4cf91acb08ef1f6168
Artem <letnev2012@gmail.com>
2022-08-07 18:29:43 +0300

    Add solution without using methods

--- Task_for_exam/Program.cs
+++ Task_for_exam/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// The Task
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Solution without using methods
+
+string[] standartArray = { "3213", "22222", "W", "=3", "543" };
+int lengthFinishArray = 0;
+
+
+for (int i = 0; i < standartArray.Length; i++)
+{
+    if (standartArray[i].Length < 4)
+    {
+        lengthFinishArray++;
+    }
+}
+string[] finishArray = new string[lengthFinishArray];
+
+int count = 0;
+for (int i = 0; i < standartArray.Length; i++)
+{
+    if (standartArray[i].Length < 4)
+    {
+        finishArray[count] = standartArray[i];
+        count++;
+    }
+}
+
+Console.WriteLine($"[{String.Join("; ", finishArray)}]");
+
+
+
